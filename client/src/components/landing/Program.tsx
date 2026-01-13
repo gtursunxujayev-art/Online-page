42285ca6ae2ce7cb63197a3d709f229a66cc63e3
gtursunxujayev-art <gtursunxujayev@gmail.com>
2026-01-13 15:23:33 +0500

    Update Program.tsx

--- client/src/components/landing/Program.tsx
+++ client/src/components/landing/Program.tsx
@@ -17,7 +17,7 @@ export default function Program() {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-2 gap-8">
           {content.program.weeks.map((item, i) => (
             <motion.div
               key={i}
