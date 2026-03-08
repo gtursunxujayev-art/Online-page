f31a2b5fd3529e893689efcbcd6fbf47ebdf6e96
gtursunxujayev-art <gtursunxujayev@gmail.com>
2026-03-08 18:05:22 +0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,31 @@
 import { Switch, Route, Router } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ContentProvider } from "@/lib/contentContext";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
-import AdminPage from "@/pages/admin123456789";
-import LoginPage from "@/pages/login";
 import { useHashLocation } from "@/lib/hashLocation";
 
 function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/admin123456789" component={AdminPage} />
-      <Route component={Home} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ContentProvider>
-        <TooltipProvider>
-          <Toaster />
-          {/* Use Hash Routing to support static deployment */}
-          <Router hook={useHashLocation}>
-            <AppRouter />
-          </Router>
-        </TooltipProvider>
-      </ContentProvider>
-    </QueryClientProvider>
+    <ContentProvider>
+      <TooltipProvider>
+        <Toaster />
+        {/* Use Hash Routing to support static deployment */}
+        <Router hook={useHashLocation}>
+          <AppRouter />
+        </Router>
+      </TooltipProvider>
+    </ContentProvider>
   );
 }
 
